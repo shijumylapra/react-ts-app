4f245ada49909f8dc6aba187e4275b3ffe7f3d20
shijumylapra <shijumylapra@gmail.com>
2025-05-26 20:09:29 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+ 
+import "./App.css";
+import Typography from "./components/atom/typography/Typography";
+
+function App(){
+   return <>   
+         <Typography/>     
+      </>;
+}
+  
+export default App;
+
---
+++ src/components/atom/typography/Typography.tsx
@@ -0,0 +1,14 @@
+
+const Typography = () => {
+  return <div> Site under constrution 
+   
+  </div>;
+    
+};
+
+
+export default Typography;
+
+//rafce
+
+
