2dca88a9058fff74e7cade6dfd61cbe6d2bda981
shijumylapra <shijumylapra@gmail.com>
2025-05-26 21:16:24 -0600

    new react js app

--- src/components/atom/typography/Typography.tsx
+++ src/components/atom/typography/Typography.tsx
@@ -1,11 +1,18 @@
 
+
 const Typography = () => {
-  return <div> Site under constrution 
-   
+  return <div style={{ backgroundImage: "url(/src/image/SUC.jpg)",
+    height: "100vh",
+            marginTop: "-70px",
+            fontSize: "50px",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+   }}> 
+    Site under constrution 
   </div>;
-    
 };
 
+ 
 
 export default Typography;
 
